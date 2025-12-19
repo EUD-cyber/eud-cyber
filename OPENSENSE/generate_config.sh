42a6e26d8c4429147641c2195ca8b2148f57b58c
mmjo-80 <mmjo@mercantec.dk>
2025-12-19 18:23:49 +0100

    Update generate_config.sh

--- OPENSENSE/generate_config.sh
+++ OPENSENSE/generate_config.sh
@@ -22,6 +22,9 @@ LAN_CIDR="24"
 # Default password: opnsense
 ROOT_PASSWORD_HASH='$2y$10$u1rPZJvM0Qz9sPZP6UZZ6OaGxK2p2pF0XjU5O4Hn5qjz'
 
+# Print pwd and store it as sting
+PATH=($pwd)
+
 if [ "$MODE" = "1" ]; then
   WAN_IP_BLOCK="<ipaddr>dhcp</ipaddr>"
   WAN_GATEWAY_BLOCK=""
@@ -45,7 +48,7 @@ else
   exit 1
 fi
 
-cat > /root/opnsense/config.xml <<EOF
+cat > $PATH"/config.xml" <<EOF
 <?xml version="1.0"?>
 <opnsense>
   <system>
