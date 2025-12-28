722f319a6a553343027b0441318cfd84cc134e91
mmjo-80 <mmjo@mercantec.dk>
2025-12-28 13:55:39 +0100

    Update generate_config.sh

--- OPNSENSE/generate_config.sh
+++ OPNSENSE/generate_config.sh
@@ -8,8 +8,10 @@ echo "1) DHCP"
 echo "2) Static"
 read -rp "Select WAN mode [1-2]: " MODE
 
-WAN_IF="vtnet0"
-LAN_IF="vtnet1"
+WAN_IF="vtnet1"
+LAN_IF="vtnet0"
+LAN2_IF="vtnet2"
+OOBM_IF="vtnet3"
 
 HOSTNAME="opnsense"
 DOMAIN="local"
@@ -18,6 +20,16 @@ TIMEZONE="UTC"
 # LAN defaults
 LAN_IP="192.168.1.1"
 LAN_CIDR="24"
+
+# LAN2 defaults
+LAN2_IP="192.168.2.1"
+LAN2_CIDR="24"
+
+# OOBM defaults
+OOBM_IP="172.20.0.2"
+OOBM_CIDR="24"
+
+WAN_DNS=""
 
 # Default password: opnsense
 ROOT_PASSWORD_HASH='$2y$10$u1rPZJvM0Qz9sPZP6UZZ6OaGxK2p2pF0XjU5O4Hn5qjz'
@@ -29,6 +41,7 @@ elif [ "$MODE" = "2" ]; then
   read -rp "WAN IP address: " WAN_IP
   read -rp "WAN CIDR (e.g. 24): " WAN_CIDR
   read -rp "WAN Gateway: " WAN_GW
+  read -rp "WAN dns: " WAN_DNS
 
   WAN_IP_BLOCK="<ipaddr>${WAN_IP}</ipaddr>
       <subnet>${WAN_CIDR}</subnet>"
@@ -74,7 +87,7 @@ cat > $(pwd)/OPNSENSE/iso/conf/config.xml <<EOF
       <permitrootlogin>1</permitrootlogin>
       <passwordauth>1</passwordauth>
     </ssh>
-    <dnsserver>${DNS_SERVER}</dnsserver>
+    <dnsserver>${WAN_DNS}</dnsserver>
   </system>
 
   <interfaces>
@@ -82,15 +95,33 @@ cat > $(pwd)/OPNSENSE/iso/conf/config.xml <<EOF
       <enable>1</enable>
       <if>${WAN_IF}</if>
       ${WAN_IP_BLOCK}
-      <descr>WAN Gateway</descr>
+      <descr>wan</descr>
     </wan>
 
     <lan>
       <enable>1</enable>
       <if>${LAN_IF}</if>
+      <descr>lan1</descr>
       <ipaddr>${LAN_IP}</ipaddr>
       <subnet>${LAN_CIDR}</subnet>
     </lan>
+
+    <opt1>
+      <enable>1</enable>
+      <if>${LAN2_IF}</if>
+      <descr>lan2</descr>
+      <ipaddr>${LAN2_IP}</ipaddr>
+      <subnet>${LAN2_CIDR}</subnet>
+    </opt1>
+    <opt2>
+      <enable>1</enable>
+      <if>${OOBM_IF}</if>
+      <descr>oobm</descr>
+      <ipaddr>${OOBM_IP}</ipaddr>
+      <subnet>${OOBM_CIDR}</subnet>
+    </opt2>
+    
+    
   </interfaces>
 
   ${WAN_GATEWAY_BLOCK}
