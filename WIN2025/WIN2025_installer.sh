15b4641abe0106733f80a89b05560133eb6e483a
mmjo-80 <mmjo@mercantec.dk>
2026-01-05 08:59:00 +0100

    Update WIN2025_installer.sh

--- WIN2025/WIN2025_installer.sh
+++ WIN2025/WIN2025_installer.sh
@@ -1,1 +1,109 @@
+#!/bin/bash
+set -e
 
+LOGFILE="$(pwd)/LOGS/WIN2025.log"
+
+# Create log file and ensure permissions
+touch "$LOGFILE"
+chmod 600 "$LOGFILE"
+
+# Redirect all output (stdout + stderr) to log AND console
+exec > >(tee -a "$LOGFILE") 2>&1
+
+echo "===== WIN2025 installation started at $(date) ====="
+
+
+START_VMID=100
+BASE_NAME="WIN2025"
+ISO_STORAGE="local"
+DISK_STORAGE="local-lvm"
+IMG_URL="https://go.microsoft.com/fwlink/?linkid=2345730&clcid=0x409&culture=en-us&country=us"
+IMG_NAME="en-us_windows_server_2025_x64_dvd_b7ec10f3.iso"
+IMG_PATH="$(pwd)/$IMG_NAME"
+VIRTIO_URL="https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.285-1/virtio-win-0.1.285.iso"
+VIRTIO_NAME=virtio-win.iso
+VIRTIO_PATH="$(pwd)/$VIRTIO_NAME"
+MEMORY=8192       # in MB
+CORES=4
+DISK_SIZE="40"
+BRIDGE="lan2"
+BRIDGE1="oobm"
+ISO_DIR="/var/lib/vz/template/iso"
+
+DST_WIN2025_PATH="$ISO_DIR}/$IMG_NAME"
+DST_VIRTIO_PATH=""$ISO_DIR}/$VIRTIO_NAME"
+
+
+# ===== Find next free VMID =====
+VMID=$START_VMID
+while qm status $VMID &>/dev/null; do
+    VMID=$((VMID + 1))
+done
+echo "Selected free VMID: $VMID"
+
+# ===== Handle VM name collision =====
+VM_NAME="$BASE_NAME"
+COUNT=1
+while qm list | awk '{print $2}' | grep -x "$VM_NAME" &>/dev/null; do
+    VM_NAME="${BASE_NAME}-${COUNT}"
+    COUNT=$((COUNT + 1))
+done
+echo "VM name to use: $VM_NAME"
+
+# ===== Download VIRTIO if missing =====
+if [ ! -f "$VIRTIO_PATH" ]; then
+    echo "Downloading $VIRTIO_NAME IMG..."
+    wget --show-progress -O "$VIRTIO_PATH" "$VIRTIO_URL"
+else
+    echo "IMG already exists: $VIRTIO_PATH"
+fi
+
+# ===== Download IMG if missing =====
+if [ ! -f "$IMG_PATH" ]; then
+    echo "Downloading $IMG_NAME IMG..."
+    wget --show-progress -O "$IMG_PATH" "$IMG_URL"
+else
+    echo "IMG already exists: $IMG_PATH"
+fi
+
+# Check if WIN2025 ISO exits in iso folder
+if [[ -f "$DST_WIN2025_PATH" ]]; then
+  echo "WIN2025 ISO already exists: $DST_WIN2025_PATH"
+else
+  echo "WIN2025 ISO not found. Copying..."
+  cp "$IMG_PATH" "$DST_WIN2025_PATH"
+  echo "User-data copied to $DST_WIN2025_PATH"
+  
+# Check if VIRTIO ISO exits in iso folder
+if [[ -f "$DST_VIRTIO_PATH" ]]; then
+  echo "VIRTIO ISO already exists: $DST_VIRTIO_PATH"
+else
+  echo "VIRTIO ISO not found. Copying..."
+  cp "$VIRTIO_PATH" "$DST_VIRTIO_PATH"
+  echo "User-data copied to $DST_
+
+
+fi
+echo "Done."
+
+qm create $VMID \
+  --name $VM_NAME \
+  --memory $MEMORY \
+  --cores $CORES \
+  --cpu host \
+  --net0 virtio,bridge=$BRIDGE \
+  --net1 virtio,bridge=$BRIDGE1 \
+  --scsihw virtio-scsi-pci \
+  --ostype win11
+
+qm set $VMID --ide2 $ISO_STORAGE:iso/$IMG_NAME,media=cdrom
+qm set $VMID --ide3 $ISO_STORAGE:iso/$VIRTIO_NAME,media=cdrom
+
+qm set $VMID --scsi0 $DISK_STORAGE:$DISK_SIZE
+qm set $VMID --boot order=scsi0
+qm set $VMID --machine q35
+qm set $VMID --bios ovmf
+qm set $VMID --efidisk0 $DISK_STORAGE:0,pre-enrolled-keys=1
+qm set $VMID --tablet 0
+
+qm start VMID
