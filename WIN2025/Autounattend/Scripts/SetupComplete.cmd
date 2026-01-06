528a92ee651876fb99e1fcdffb9307832496a116
mmjo-80 <mmjo@mercantec.dk>
2026-01-06 21:47:05 +0100

    Update SetupComplete.cmd

--- WIN2025/Autounattend/Scripts/SetupComplete.cmd
+++ WIN2025/Autounattend/Scripts/SetupComplete.cmd
@@ -1,6 +1,7 @@
 @echo off
-echo Running SetupComplete...
+echo SetupComplete starting... > C:\Windows\Setup\Scripts\post.log
 
-powershell.exe -ExecutionPolicy Bypass -File "C:\Windows\Setup\Scripts\post.ps1"
+powershell.exe -ExecutionPolicy Bypass -NoProfile -File "C:\Windows\Setup\Scripts\post.ps1" >> C:\Windows\Setup\Scripts\post.log 2>&1
 
+echo Done. >> C:\Windows\Setup\Scripts\post.log
 exit /b 0
