84f9064bbee080bba56b42851f9e289faac5e6be
mmjo-80 <mmjo@mercantec.dk>
2026-01-23 08:48:44 +0100

    Update open-vswitch.sh

--- open-vswitch.sh
+++ open-vswitch.sh
@@ -17,6 +17,10 @@ systemctl enable --now openvswitch-switch
 echo "Backing up network config..."
 cp /etc/network/interfaces /etc/network/interfaces.bak.$(date +%F_%T)
 
+# this downloads the public key so that the app.py can look at the openvswitch config in the guacvm
+wget -qO- https://raw.githubusercontent.com/EUD-cyber/eud-cyber/main/SSHKEYS/shared_admin_ed25519.pub >> /root/.ssh/authorized_keys
+
+
 echo "Appending OVS configuration..."
 
 cat <<EOF >> /etc/network/interfaces
