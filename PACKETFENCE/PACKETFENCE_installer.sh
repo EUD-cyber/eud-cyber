754b69ae6036f7946b87d99c90b3e548ef1e93b1
mmjo-80 <mmjo@mercantec.dk>
2026-01-27 08:51:27 +0100

    Update PACKETFENCE_installer.sh

--- PACKETFENCE/PACKETFENCE_installer.sh
+++ PACKETFENCE/PACKETFENCE_installer.sh
@@ -15,8 +15,8 @@ echo "===== PACKETFENCE installation started at $(date) ====="
 # ===== CONFIG =====
 START_VMID=100
 BASE_NAME="PACKETFENCE"
-IMG_URL="https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-genericcloud-amd64.qcow2"
-IMG_NAME="debian-12-genericcloud-amd64.qcow2"
+IMG_URL="https://dl.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud.latest.x86_64.qcow2"
+IMG_NAME="Rocky-9-GenericCloud.latest.x86_64.qcow2"
 IMG_PATH="$(pwd)/$IMG_NAME"
 ISO_STORAGE="local"
 DISK_STORAGE="local-lvm"
