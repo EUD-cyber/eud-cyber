8d9a6031ff5f30817d479365453b1268c49e9de7
mmjo-80 <mmjo@mercantec.dk>
2026-02-02 11:15:53 +0100

    Update full_install.sh

--- full_install.sh
+++ full_install.sh
@@ -125,18 +125,24 @@ case "$CHOICE" in
 
     echo 'Change proxmox repo to no-enterprise'
     bash '$REPO'
+    wall "REPO done"
 
     echo 'Checking packages and snippets...'
     bash '$PREREQ'
-
+    wall "PREREQ done"
+    
     echo 'Starting Open vSwitch installation and configuration'
     bash '$OPENVSWITCH'
+    wall "OPENVSWITCH done"
 
     echo 'Starting Guacamole VM creation'
     bash '$GUACVM'
+    wall "GuacVM done"
+
 
     echo 'Starting Client01 VM creation'
     bash '$CLIENT01'
+    wall "Client01 done"
 
     echo 'Starting Vuln-server01 VM creation'
     bash '$VULNSRV01'
