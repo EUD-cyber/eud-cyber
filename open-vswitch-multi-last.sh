c4a53e5676972d9cc38ab103c1fcd80734160239
mmjo-80 <mmjo@mercantec.dk>
2026-02-02 12:26:34 +0100

    Update and rename open-vswitch-multi.sh to open-vswitch-multi-last.sh

--- open-vswitch-multi-last.sh
+++ open-vswitch-multi-last.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-### VARIABLES (EDIT THESE) ###
-BRIDGE1=""
-BRIDGE2=""
-BRIDGE3=""
-
 ### CHECK ROOT ###
 if [[ $EUID -ne 0 ]]; then
   echo "Run as root"
@@ -19,29 +14,6 @@ cp /etc/network/interfaces /etc/network/interfaces.bak.$(date +%F_%T)
 
 # this downloads the public key so that the app.py can look at the openvswitch config in the guacvm
 wget -qO- https://raw.githubusercontent.com/EUD-cyber/eud-cyber/main/SSHKEYS/shared_admin_ed25519.pub >> /root/.ssh/authorized_keys
-
-
-echo "Appending OVS configuration..."
-
-cat <<EOF >> /etc/network/interfaces
-
-# ===== Open vSwitch configuration =====
-
-auto $BRIDGE1
-iface $BRIDGE1 inet static
-    ovs_type OVSBridge
-
-auto $BRIDGE2
-iface $BRIDGE2 inet static
-    ovs_type OVSBridge
-
-auto $BRIDGE3
-iface $BRIDGE3 inet static
-    ovs_type OVSBridge
-    address 172.20.0.100/24
-
-# ===== End Open vSwitch configuration =====
-EOF
 
 echo "Reloading network configuration..."
 ifreload -a
