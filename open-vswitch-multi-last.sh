ffb9573298aad61937e30840450059fc028b46d6
mmjo-80 <mmjo@mercantec.dk>
2026-02-04 08:24:41 +0100

    Update open-vswitch-multi-last.sh

--- open-vswitch-multi-last.sh
+++ open-vswitch-multi-last.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
 
+cat <<EOF >> /etc/network/interfaces
+
+# ===== Open vSwitch configuration (prox oobm) =====
+
 auto prox_oobm
 iface prox_oobm inet manual
     ovs_type OVSBridge
     address 172.30.0.100/24
+    
+# ===== End Open vSwitch configuration (prox oobm) =====
+EOF
+
 
 echo "Reloading network configuration..."
 ifreload -a
