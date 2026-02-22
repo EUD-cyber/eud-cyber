c1e011fdd06b0b9b8a3eca13e0b81f4b7c06b57e
mmjo-80 <mmjo@mercantec.dk>
2026-02-22 12:24:00 +0100

    Update WIN2025_multi_installer.sh

--- WIN2025/WIN2025_multi_installer.sh
+++ WIN2025/WIN2025_multi_installer.sh
@@ -24,8 +24,8 @@ echo "===== WIN2025 installation started at $(date) ====="
 
 START_VMID=$((LAB * 1000))
 BASE_NAME="lab${LAB}-WIN2025"
-ISO_STORAGE="local"
-DISK_STORAGE="local-lvm"
+ISO_STORAGE="${LOCAL:-local}"
+DISK_STORAGE="${LVM:-local-lvm}"
 IMG_URL="https://go.microsoft.com/fwlink/?linkid=2345730&clcid=0x409&culture=en-us&country=us"
 IMG_NAME="en-us_windows_server_2025_x64_dvd_b7ec10f3.iso"
 IMG_PATH="$(pwd)/IMAGES/$IMG_NAME"
