72e72b114b0a78c0cc61178918bd09ed56fce0c2
mmjo-80 <mmjo@mercantec.dk>
2026-02-22 11:16:21 +0100

    Update GUACVM_multi_installer.sh

--- GUACVM/GUACVM_multi_installer.sh
+++ GUACVM/GUACVM_multi_installer.sh
@@ -30,7 +30,7 @@ exec > >(tee -a "$LOGFILE") 2>&1
 echo "===== GUACVM installation started at $(date) ====="
 
 # ===== CONFIG =====
-START_VMID=$((LAB * 100))
+START_VMID=$((LAB * 1000))
 BASE_NAME="lab${LAB}-guacvm"
 IMG_URL="${LINUX_IMG:-https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img}"
 IMG_NAME="noble-server-cloudimg-amd64.img"
