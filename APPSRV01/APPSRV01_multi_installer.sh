d8601abf75a1a003cd77e5851e068aa5f36c3d5d
mmjo-80 <mmjo@mercantec.dk>
2026-02-22 10:38:15 +0100

    Update APPSRV01_multi_installer.sh

--- APPSRV01/APPSRV01_multi_installer.sh
+++ APPSRV01/APPSRV01_multi_installer.sh
@@ -23,7 +23,8 @@ echo "===== APPSRV01 installation started at $(date) ====="
 # ===== CONFIG =====
 START_VMID=$((LAB * 100))
 BASE_NAME="lab${LAB}-APPSRV01"
-IMG_URL="https://cloud-images.ubuntu.com/noble/20251213/noble-server-cloudimg-amd64.img"
+#IMG_URL="https://cloud-images.ubuntu.com/noble/20251213/noble-server-cloudimg-amd64.img"
+IMG_URL="https://cloud-images.ubuntu.com/noble/20260217/noble-server-cloudimg-amd64.img"
 IMG_NAME="noble-server-cloudimg-amd64.img"
 IMG_PATH="$(pwd)/$IMG_NAME"
 ISO_STORAGE="local"
