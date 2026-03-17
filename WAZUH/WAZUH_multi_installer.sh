38aabf56cdaa7404f8a81247c46ba8c7740ab270
mmjo-80 <mmjo@mercantec.dk>
2026-03-17 12:33:56 +0100

    Create WAZUH_multi_installer.sh

---
+++ WAZUH/WAZUH_multi_installer.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+LAB="$1"
+
+if [[ -z "$LAB" ]] || ! [[ "$LAB" =~ ^[0-9]+$ ]]; then
+  echo "Usage: $0 <lab-number>"
+  exit 1
+fi
+
+LOGFILE="$(pwd)/LOGS/WAZUH_lab${LAB}.log"
+
+# Create log file and ensure permissions
+touch "$LOGFILE"
+chmod 600 "$LOGFILE"
+
+# Redirect all output (stdout + stderr) to log AND console
+exec > >(tee -a "$LOGFILE") 2>&1
+
+echo "===== WAZUH installation started at $(date) ====="
+
+# ===== CONFIG =====
+START_VMID=$((LAB * 1000))
+BASE_NAME="lab${LAB}-WAZUH"
+IMG_URL="${LINUX_IMG:-https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img}"
+IMG_NAME="noble-server-cloudimg-amd64.img"
+IMG_PATH="$(pwd)/$IMG_NAME"
+ISO_STORAGE="${LOCAL:-local}"
+DISK_STORAGE="${LVM:-local-lvm}"
+MEMORY=16384       # in MB
+CORES=4
+SOCKETS=2
+DISK_SIZE="80G"    # the number is in GB
+BRIDGE="lab${LAB}_lan2"
+BRIDGE1="lab${LAB}_oobm"
+IP_ADDR="ip=192.168.2.20/24"
+DNS_SERVER="192.168.2.1"
+IP_GW="gw=192.168.2.1"
+OOBM_IP="ip=172.20.0.20/24"
+SNIPPET_DIR="/var/lib/vz/snippets"
+SRC_USERDATA="$(pwd)/WAZUH/WAZUH_userdata.yaml"     # source file
+DST_USERDATA="WAZUH_userdata_lab${LAB}.yaml"            # destination filename
+
+DST_PATH="${SNIPPET_DIR}/${DST_USERDATA}"
+
+echo "Checking Cloud-Init user-data snippet..."
+
+# Check if snippet already exists
+if [[ -f "$DST_PATH" ]]; then
+  echo "User-data already exists: $DST_PATH"
+else
+  echo "User-data not found. Copying..."
+  cp "$SRC_USERDATA" "$DST_PATH"
+  chmod 644 "$DST_PATH"
+  echo "User-data copied to $DST_PATH"
+fi
+echo "Done."
+
+# ===== Find next free VMID =====
+VMID=$START_VMID
+while qm status $VMID &>/dev/null; do
+    VMID=$((VMID + 1))
+done
+echo "Selected free VMID: $VMID"
+
+# ===== Handle VM name collision =====
+VM_NAME="$BASE_NAME"
+COUNT=1
+while qm list | awk '{print $2}' | grep -x "$VM_NAME" &>/dev/null; do
+    VM_NAME="${BASE_NAME}-${COUNT}"
+    COUNT=$((COUNT + 1))
+done
+echo "VM name to use: $VM_NAME"
+
+# ===== Download IMG if missing =====
+if [ ! -f "$IMG_PATH" ]; then
+    echo "Downloading $IMG_NAME IMG..."
+    wget --show-progress -O "$IMG_PATH" "$IMG_URL"
+else
+    echo "IMG already exists: $IMG_PATH"
+fi
+
+# ===== Create VM =====
+echo "Creating VM $VMID..."
+qm create $VMID \
+  --name "$VM_NAME" \
+  --memory $MEMORY \
+  --cores $CORES \
+  --sockets $SOCKETS \
+  --cpu host \
+  --net0 virtio,bridge=$BRIDGE \
+  --net1 virtio,bridge=$BRIDGE1 \
+  --ostype l26
+
+# ===== Add LVM disk =====
+qm importdisk $VMID $IMG_NAME $DISK_STORAGE
+qm set $VMID \
+  --scsihw virtio-scsi-pci \
+  --scsi0 ${DISK_STORAGE}:"vm-$VMID-disk-0"
+
+qm disk resize $VMID scsi0 +$DISK_SIZE
+
+# ===== Boot order and console =====
+qm set $VMID \
+  --ide2 $DISK_STORAGE:cloudinit \
+  --boot c \
+  --bootdisk scsi0 \
+
+# ===== Enable QEMU Guest Agent =====
+qm set $VMID --agent enabled=1
+
+# ===== Set autostart =====
+qm set $VMID --onboot 1
+
+# ===== Cloud-init =====
+qm set $VMID --ipconfig0 $IP_ADDR,$IP_GW \
+  --ipconfig1 $OOBM_IP \
+  --searchdomain cloud.local \
+  --nameserver $DNS_SERVER \
+  --ciupgrade \
+  --cicustom "user=$ISO_STORAGE:snippets/${DST_USERDATA}"
+
+# ===== Start VM =====
+echo "Starting VM $VMID ($VM_NAME)..."
+qm start $VMID
+
+echo "VM $VMID ($VM_NAME) started successfully!"
+
+echo "Waiting for VM to power off..."
+
+while true; do
+  STATUS=$(qm status "$VMID" | awk '{print $2}')
+
+  if [[ "$STATUS" == "stopped" ]]; then
+    echo "VM is powered off."
+    break
+  fi
+
+  sleep 5
+done
+
+echo "Creating snapshot..."
+qm snapshot "$VMID" First_snapshot --description "Clean baseline snapshot for lab reset"
+
+echo "Snapshot created successfully."
+
+echo "Starting VM again..."
+qm start "$VMID"
+
+echo "VM started."
